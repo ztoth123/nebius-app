f6767f7bd08e6271f1c460edbae2fe6cf4692c09
Zoltan Toth <ztoth123@gmail.com>
2023-03-04 16:12:24 +0100

    the routing table for igw is added

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -24,7 +24,23 @@ resource "aws_internet_gateway" "main-gw" {
   vpc_id = aws_vpc.main.id
 }
 
+resource "aws_route_table" "main-rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main-gw.id
+  }
+}
 
+resource "aws_route_table_association" "subnet1-to-main-rt" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.main-rt.id
+}
+
+resource "aws_route_table_association" "subnet2-to-main-rt" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.main-rt.id
+}
 
 resource "aws_security_group" "alb-sg" {
   name        = "alb-sg"
